794b809494fd49c5b8c44ad3a6bbd499dbd598e0
Geonovum <>
2022-12-21 09:23:04 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/aa6381212c3863c065f98f8ca7a288fa0d7aa2ca

---
+++ js/config.js
@@ -0,0 +1,28 @@
+var respecConfig =
+{
+  pubDomain: "wp",
+  specStatus: "GN-WV",
+  specType: "AL",
+  license: "cc-by-nd",
+  publishDate: "2022-12-21",
+  authors: [
+    {
+      name: "Michel Grothe",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Michel Grothe",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  shortName: "VerkenningDataspaces",
+  format: "markdown",
+  github: "https://github.com/Geonovum/wp-VerkenningDataspaces/",
+  issueBase: "https://github.com/Geonovum/wp-VerkenningDataspaces/issues/",
+  edDraftURI: "https://Geonovum.github.io/wp-VerkenningDataspaces/",
+  generatedSubtitle: "Werkversie 21 december 2022"
+}
